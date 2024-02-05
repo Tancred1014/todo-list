18c4ff49e9806f8d115491f877989fba312c66dd
Tancred1014 <tancred.shih@gmail.com.tw>
2024-02-05 14:58:15 +0800

    handle flash message by using message/error middleware

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const methodOverride = require('method-override')
 
 const router = require('./routes')
 
+const messageHandler = require('./middlewares/message-handler')
+const errorHandler = require('./middlewares/error-handler')
+
+
 const port = 3000
 
 app.engine('.hbs', engine({ extname: '.hbs' }))
@@ -24,7 +28,11 @@ app.use(session({
 }))
 app.use(flash())
 
+app.use(messageHandler)
+
 app.use(router)
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log('App is running on port 3000')
