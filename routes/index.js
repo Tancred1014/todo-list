0d1b34af7d7a0c8b2a46f2e138f577bba3ffffed
Tancred1014 <tancred.shih@gmail.com.tw>
2024-03-01 13:36:00 +0800

    implement login using passport

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,33 @@
 const express = require('express')
 const router = express.Router()
 
-// const passport = require('passport')
-// const LocalStrategy = require('passport-local')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
 
+const db = require('../models')
+const User = db.User
 
+passport.use(new LocalStrategy({ usernameField: 'email' }, (username, password, done) => {
+  return User.findOne({
+    attributes: ['id', 'name', 'email', 'password'],
+    where: { email: username },
+    raw: true
+  })
+    .then((user) => {
+      if (!user || user.password !== password) {
+        return done(null, false, { message: 'email或密碼錯誤' })
+      }
+      return done(null, user)
+    })
+    .catch((error) =>
+      error.errorMessage = '登入失敗',
+      done(error))
+}))
+
+passport.serializeUser((user, done) => {
+  const { id, name, email } = user
+  return done(null, { id, name, email })
+})
 
 // 準備引入路由模組
 const todos = require('./todos')
@@ -26,9 +49,11 @@ router.get('/login', (req, res) => {
   return res.render('login')
 })
 
-router.post('login', (req, res) => {
-  return res.render('req.body')
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/todos',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 router.post('logout', (req, res) => {
   return res.render('logout')
